e920c7cce1261937046a1b9af481f6bc8a2c0aa2
msd <moin.salahuddin@gmail.com>
2017-03-22 11:42:04 +0100

    created token manually

--- auth_tutorial/server/index.js
+++ auth_tutorial/server/index.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
-
+var User = require('./models/User.js')
+var jwt = require('./services/jwt.js')
 var app = express()
 
 app.use(bodyParser.json())
@@ -14,26 +15,31 @@ app.use(function(req,res,next){
     next()
 })
 
-var User = mongoose.model('User', {
-    name: String,
-    password: String
-})
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/auth')
 
 app.post('/register', function(req,res){
     var user = req.body
-    console.log(req.body)
 
-    var newUser = new User({
+    var newUser = new User.model({
         name: user.name,
         password: user.password
     })
 
-    newUser.save(function(err){
-        res.status(200).json(newUser)
-    })
-})
+    var payload = {
+        iss: req.hostname,
+        sub: user._id
+    };
 
-mongoose.connect('mongodb://localhost/auth')
+    var token = jwt.encode(payload, "mySecret...");
+
+    newUser.save(function(err){
+        res.status(200).send({
+                user: newUser.toJSON(), 
+                token: token
+            });
+        })
+    })
 
 var server = app.listen(3000, function(){
     console.log('server listening on ', server.address())
